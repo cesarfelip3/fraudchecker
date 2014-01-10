7f83871e651134ed1b1ba23b26fd75d9cdef0769
itmyprofession <itmyprofession@gmail.com>
2014-01-10 21:42:43 +0545

    show proper design error

--- app/modules/content/views/_partial/notifications.blade.php
+++ app/modules/content/views/_partial/notifications.blade.php
@@ -1,5 +1,5 @@
 @if ($message = Session::get('success'))
-<div class="alert alert-success alert-dismissable">
+<div class="alert alert-success alert-dismissable col-md-offset-3 col-md-6">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
   <strong>Success:</strong> {{ $message }}
 </div>
@@ -7,7 +7,7 @@
 @endif
 
 @if ($message = Session::get('error'))
-<div class="alert alert-danger alert-dismissable">
+<div class="alert alert-danger alert-dismissable col-md-offset-3 col-md-6">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
   <strong>Error:</strong> {{ $message }}
 </div>
@@ -15,7 +15,7 @@
 @endif
 
 @if ($message = Session::get('warning'))
-<div class="alert alert-warning alert-dismissable">
+<div class="alert alert-warning alert-dismissable col-md-offset-3 col-md-6">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
   <strong>Warning:</strong> {{ $message }}
 </div>
@@ -23,7 +23,7 @@
 @endif
 
 @if ($message = Session::get('info'))
-<div class="alert alert-info alert-dismissable">
+<div class="alert alert-info alert-dismissable col-md-offset-3 col-md-6">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
   <strong>FYI:</strong> {{ $message }}
 </div>
